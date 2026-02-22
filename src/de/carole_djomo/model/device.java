e40b80d150699c93c090432bf853a4b3089dfe77
Carole Djomo <carole.djomo@gmail.com>
2026-02-22 03:18:58 +0000

    change

---
+++ src/de/carole_djomo/model/device.java
@@ -0,0 +1,42 @@
+import javax.persistence.*;
+
+package de.carole_djomo.model;
+
+
+@Entity
+@Table(name = "device")
+public class Device {
+    private Integer id;
+    private String name;
+
+    /**
+     * Konstruktor.
+     */
+    public Device() {
+    }
+
+    /**
+     * Der Primary Key.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer anId) {
+        this.id = anId;
+    }
+
+    /**
+     * Der Name des Geräts.
+     */
+    @Column(name = "name")    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
